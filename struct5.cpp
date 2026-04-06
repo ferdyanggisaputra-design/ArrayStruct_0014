5280438b54da0dc6a1b8088bab36f57a38305a99
ferdyanggisaputra-design <ferdyanggisaputra@gmail.com>
2026-04-06 12:10:15 +0700

    menambahkan getline

--- struct5.cpp
+++ struct5.cpp
@@ -15,3 +15,19 @@ struct mahasiswa
     AlamatDetail alamat;
     int umur;
 };
+
+int main(){
+        // deklarasi variable struct
+    mahasiswa mhs[3];
+    for(int i = 0; i <=2; i++){
+    // mengisi data
+    cout << "Isikan data nama : ";
+    getline(cin, mhs[i].nama);
+    cout << "Isikan data desa : ";
+    getline(cin, mhs[i].alamat.desa);
+    cout << "Isikan data kota : ";
+    getline(cin, mhs[i].alamat.kota);
+    cout << "Isikan data usia : ";
+    cin >> mhs[i].umur;
+    cin.ignore();
+    }
