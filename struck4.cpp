bc790d0da77d7fe5d25f3e75ff2214edc7a75cac
ferdyanggisaputra-design <ferdyanggisaputra@gmail.com>
2026-04-06 12:04:07 +0700

    deklarasi varieable struct

--- struck4.cpp
+++ struck4.cpp
@@ -8,3 +8,22 @@ struct AlamatDetail
     string kota;
     
 };
+struct mahasiswa
+{
+    string nama;
+    AlamatDetail alamat;
+    int umur;
+};
+int main(){
+        // deklarasi variable struct
+    mahasiswa mhs;
+
+    // mengisi data
+    cout << "Isikan data nama : ";
+    getline(cin, mhs.nama);
+    cout << "Isikan data desa : ";
+    getline(cin, mhs.alamat.desa);
+    cout << "Isikan data kota : ";
+    getline(cin, mhs.alamat.kota);
+    cout << "Isikan data usia : ";
+    cin >> mhs.umur;
